a5aea27957ffa3b0ecc7cd749841cf6a3b7f10a1
jho0902 <!Jho@2002#GitHub>
2025-06-20 15:52:57 -0300

    Criado a Entidade User

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,19 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+}
